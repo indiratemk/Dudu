09b209c256d4fd857a1248d0409e1afef4179d3d
indiratemk <123bylbhf>
2021-07-09 00:04:36 +0300

    change methods output on task model

--- app/src/main/java/com/example/dudu/data/local/DuduDatabase.kt
+++ app/src/main/java/com/example/dudu/data/local/DuduDatabase.kt
@@ -2,6 +2,7 @@ package com.example.dudu.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.example.dudu.data.local.entities.TaskEntity
 
 @Database(entities = [TaskEntity::class], version = 1, exportSchema = false)
 abstract class DuduDatabase : RoomDatabase() {
--- app/src/main/java/com/example/dudu/data/local/LocalDataSource.kt
+++ app/src/main/java/com/example/dudu/data/local/LocalDataSource.kt
@@ -1,25 +1,43 @@
 package com.example.dudu.data.local
 
+import com.example.dudu.data.helpers.mapFromEntityToTask
+import com.example.dudu.data.helpers.mapFromTaskToEntity
+import com.example.dudu.data.models.Task
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 
 class LocalDataSource(
     private val taskDao: TaskDao
 ) {
 
-    fun getTasks(showDone: Boolean): Flow<List<TaskEntity>> {
-        return taskDao.getTasks(showDone)
+    fun getTasks(showDone: Boolean): Flow<List<Task>> {
+        return flow {
+            emitAll(taskDao.getTasks(showDone).map { tasks ->
+                tasks.map { mapFromEntityToTask(it) }
+            })
+        }
     }
 
     fun getDoneTasksCount(): Flow<Int> {
         return taskDao.getDoneTasksCount()
     }
 
-    suspend fun addTask(task: TaskEntity) {
-        taskDao.insertTask(task)
+    suspend fun getTask(taskId: String): Task {
+        return mapFromEntityToTask(taskDao.getTask(taskId))
     }
 
-    suspend fun updateTask(task: TaskEntity) {
-        taskDao.updateTask(task)
+    suspend fun addTask(task: Task) {
+        taskDao.insertTask(mapFromTaskToEntity(task))
+    }
+
+    suspend fun updateTask(task: Task) {
+        taskDao.updateTask(mapFromTaskToEntity(task))
+    }
+
+    suspend fun removeTask(task: Task) {
+        taskDao.deleteTask(mapFromTaskToEntity(task))
     }
 
     suspend fun removeTask(task: TaskEntity) {
