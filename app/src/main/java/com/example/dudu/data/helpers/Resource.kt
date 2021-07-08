2e2feab0b275e305c24bb53c211f329ec6949674
indiratemk <123bylbhf>
2021-07-09 00:02:35 +0300

    change resource states

--- app/src/main/java/com/example/dudu/data/helpers/Resource.kt
+++ app/src/main/java/com/example/dudu/data/helpers/Resource.kt
@@ -2,8 +2,10 @@ package com.example.dudu.data.helpers
 
 sealed class Resource<out T: Any> {
 
-    data class Success<out T: Any>(val data: T) : Resource<T>()
+    data class Loading<out T: Any>(val data: T) : Resource<T>()
 
-    // TODO: 7/7/21 изменить
+    data class Loaded<out T: Any>(val data: T) : Resource<T>()
+
+    // TODO: 7/9/21 изменить эксепшн
     data class Error(val exception: Exception) : Resource<Nothing>()
 }
