2360cb8f4dc680aae9d4cfe1c1bddeae24e896e4
indiratemk <123bylbhf>
2021-07-18 20:33:43 +0300

    override equals

--- app/src/main/java/com/example/dudu/data/models/Task.kt
+++ app/src/main/java/com/example/dudu/data/models/Task.kt
@@ -10,4 +10,26 @@ data class Task(
     val deadline: Long,
     val priority: String,
     val isDone: Boolean
-) : Parcelable
+) : Parcelable {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other)
+            return true
+        if (other !is Task)
+            return false
+        return this.id == other.id &&
+                this.isDone == other.isDone &&
+                this.description == other.description &&
+                this.deadline == other.deadline &&
+                this.priority == other.priority
+    }
+
+    override fun hashCode(): Int {
+        var result = id.hashCode()
+        result = 31 * result + description.hashCode()
+        result = 31 * result + deadline.hashCode()
+        result = 31 * result + priority.hashCode()
+        result = 31 * result + isDone.hashCode()
+        return result
+    }
+}
